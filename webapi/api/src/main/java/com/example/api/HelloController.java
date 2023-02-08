5010778ad50410b9cb05651e2984c5f6055e36ba
kentny <kentaro.terasaki@gmail.com>
2023-02-08 15:27:23 +0900

    SpringBootの初期設定およびHelloControllerの実装

---
+++ webapi/api/src/main/java/com/example/api/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(
+            @RequestParam String lang
+    ) {
+        return "HELLO";
+    }
+}
